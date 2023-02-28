9501b8ca5393c7648f0abab017bfc506a7ee6c0e
Armando <dreiteilig.backoefen_0f@icloud.com>
2023-02-28 23:31:06 +0100

    Okay now its done fr (I feel like a designer)

--- 2-practice/traffic_lights_simulator/src/index.js
+++ 2-practice/traffic_lights_simulator/src/index.js
@@ -5,9 +5,10 @@ import TrafficLightCasing from "./trafficLightCasing/trafficLightCasing";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <TrafficLightCasing />
-  </React.StrictMode>
+    <React.StrictMode>
+        <TrafficLightCasing/>
+        <TrafficLightCasing/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- 2-practice/traffic_lights_simulator/src/trafficLightCasing/trafficLightCasing.js
+++ 2-practice/traffic_lights_simulator/src/trafficLightCasing/trafficLightCasing.js
@@ -3,12 +3,14 @@ import './trafficLightCasing.css'
 import {useState, useEffect} from "react";
 
 const TrafficLightCasing = (props) => {
-
-    const [lightProps, setLightProps] = useState([
-        {color: "red", active: true},
+    const newLightProps = [
+        {color: "red", active: false},
         {color: "yellow", active: false},
         {color: "green", active: false}
-    ]);
+    ];
+    newLightProps[Math.floor(Math.random() * newLightProps.length)].active = true;
+
+    const [lightProps, setLightProps] = useState(newLightProps);
 
     const nextStep = () => setLightProps((prevState) => {
         const newState = [...prevState];
@@ -18,44 +20,47 @@ const TrafficLightCasing = (props) => {
         return newState;
     });
 
+    const timeout = Math.floor(Math.random() * 1500) + 500;
+
     useEffect(() => {
         const intervalId = setInterval(() => {
             nextStep();
-        }, 1500);
+        }, timeout);
         return () => clearInterval(intervalId);
-    }, []);
+    }, [timeout]);
 
     const handleClick = () => {
         nextStep();
     }
 
     return (
-        <div className="traffic-light-casing"
-             onClick={handleClick}
-             style={{
-                 backgroundColor: props.color ? props.color : "gray",
-                 width: props.width ? props.width : "120px",
-                 height: props.height ? props.height : "fit-content",
-                 borderRadius: props.borderRadius ? props.borderRadius : "10px",
-                 border: props.border ? props.border : "1px solid black",
-                 boxShadow: props.boxShadow ? props.boxShadow : "0 0 0 0",
-                 margin: props.margin ? props.margin : "10px auto",
-                 padding: props.padding ? props.padding : "10px",
-                 display: props.display ? props.display : "flex",
-                 flexDirection: props.flexDirection ? props.flexDirection : "column",
-                 justifyContent: props.justifyContent ? props.justifyContent : "space-between",
-                 alignItems: props.alignItems ? props.alignItems : "center",
-                 flexWrap: props.flexWrap ? props.flexWrap : "nowrap",
-                 alignContent: props.alignContent ? props.alignContent : "stretch",
-                 position: props.position ? props.position : "relative",
-                 top: props.top ? props.top : "0",
-                 right: props.right ? props.right : "0",
-                 bottom: props.bottom ? props.bottom : "0",
-                 left: props.left ? props.left : "0",
-                 zIndex: props.zIndex ? props.zIndex : "0"
-             }}
-        >
-            <TrafficLight lightProps={lightProps}/>
+        <div className="traffic-light-casing-container">
+            <div className="traffic-light-casing"
+                 onClick={handleClick}
+                 style={{
+                     backgroundColor: props.color ? props.color : "gray",
+                     width: props.width ? props.width : "120px",
+                     height: props.height ? props.height : "fit-content",
+                     borderRadius: props.borderRadius ? props.borderRadius : "10px",
+                     border: props.border ? props.border : "1px solid black",
+                     boxShadow: props.boxShadow ? props.boxShadow : "0 0 0 0",
+                     margin: props.margin ? props.margin : "10px",
+                     padding: props.padding ? props.padding : "10px",
+                     display: props.display ? props.display : "flex",
+                     flexWrap: props.flexWrap ? props.flexWrap : "wrap",
+                     flexDirection: props.flexDirection ? props.flexDirection : "column",
+                     justifyContent: props.justifyContent ? props.justifyContent : "space-between",
+                     alignItems: props.alignItems ? props.alignItems : "center",
+                     alignContent: props.alignContent ? props.alignContent : "stretch",
+                     top: props.top ? props.top : "0",
+                     right: props.right ? props.right : "0",
+                     bottom: props.bottom ? props.bottom : "0",
+                     left: props.left ? props.left : "0",
+                     zIndex: props.zIndex ? props.zIndex : "0",
+                 }}
+            >
+                <TrafficLight lightProps={lightProps}/>
+            </div>
         </div>
     )
 }
